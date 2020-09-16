a3b0cc2d37293f0b5c5d98f5b4e137d8f9377396
Olga Emelyanova <volha.emelianova@gmail.com>
2020-09-16 23:56:55 +0300

    Add very simple blinky, PoC-like

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
-
-int main()
-{
-  return 0;
+int main() {
+    *((unsigned int *)0x400FE608U) = 0x20U;
+    *((unsigned int *)0x4002551CU) = 0xEU;
+    *((unsigned int *)0x40025400U) = 0xEU;
+    
+    while (1) {
+        *((unsigned int *)0x400253FCU) = 0x04U;
+        int counter = 0;
+        while (counter < 2000000) {
+            ++counter;
+        }
+        
+        *((unsigned int *)0x400253FCU) = 0x00U;
+        counter = 0;
+        while (counter < 2000000) {
+            ++counter;
+        }
+    }
+    
+    return 0;
 }
