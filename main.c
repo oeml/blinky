dd28ab9d8dc97f3d96e1e58371e76c5f1a89c2f1
Olga Emelyanova <volha.emelianova@gmail.com>
2020-09-23 22:04:45 +0300

    Add header with defines for registers

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
+#include "TM4C123GH6PM.h"
+
 int main() {
-    *((unsigned int *)0x400FE608U) = 0x20U;
-    *((unsigned int *)0x4002551CU) = 0xEU;
-    *((unsigned int *)0x40025400U) = 0xEU;
+    SYSCTL_RCGCGPIO_R = 0x20U;
+    GPIO_PORTF_DEN_R = 0xEU;
+    GPIO_PORTF_DIR_R = 0xEU;
     
     while (1) {
-        *((unsigned int *)0x400253FCU) = 0x04U;
-        int counter = 0;
-        while (counter < 2000000) {
+        GPIO_PORTF_DATA_R = 0x04U;
+        int volatile counter = 0;
+        while (counter < 500000) {
             ++counter;
         }
         
-        *((unsigned int *)0x400253FCU) = 0x00U;
+        GPIO_PORTF_DATA_R = 0x00U;
         counter = 0;
-        while (counter < 2000000) {
+        while (counter < 500000) {
             ++counter;
         }
     }
